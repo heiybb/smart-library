d73f3b827ca673c4e0127f52aeb948cc112b8c0d
xychenxy <mavischen916@gmail.com>
2019-05-26 02:01:43 +1000

    Update flask_site.py

--- flask_site.py
+++ flask_site.py
@@ -1,9 +1,9 @@
-from flask import Flask, Blueprint, request, jsonify, render_template, url_for, flash, redirect
+from flask import Flask, Blueprint, request, jsonify, render_template, url_for, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os, requests, json
 from forms import RegistrationForm, LoginForm
-
+from flask_login import login_user, current_user, logout_user, login_required
 site = Blueprint("site", __name__)
 
 group = [
@@ -30,16 +30,31 @@ group = [
 @site.route("/", methods=['POST','GET','PUT','DELETE'])
 def index():
     # Use REST API.
+    if session.get('username') == 'xiaoyu' and session.get('password') == '111':
+        userinfo = 'xiaoyu'
+    else:
+        userinfo = None
+
     if request.method == 'POST':
 
         method_put = request.form['put']
         method_delete = request.form['delete']
 
+        Title = request.form["Title"]
+        Author = request.form["Author"]
+        PublishedDate = request.form["PublishedDate"]
+        ISBN = request.form["ISBN"]
+
+
+        # update method
         if method_put == "PUT":
+            if len(Title) > 100 or len(Author) > 50:
+                response = requests.get("http://127.0.0.1:5000/book")
+                data = json.loads(response.text)
+                flash('The Title or Author is too long! 1. Modify your input. 2. Contact IT suport.', 'danger')
+                return render_template("home.html", book=data)
+
             id = request.form['Id']
-            Title = request.form["Title"]
-            Author = request.form["Author"]
-            PublishedDate = request.form["PublishedDate"]
             data = {
                 "Title": Title,
                 "Author": Author,
@@ -48,21 +63,23 @@ def index():
             headers = {
                 "Content-type": "application/json"
             }
-            response = requests.put("http://127.0.0.1:5000/book/"+id, data=json.dumps(data), headers=headers)
+            requests.put("http://127.0.0.1:5000/book/"+id, data=json.dumps(data), headers=headers)
+            response = requests.get("http://127.0.0.1:5000/book")
+            data = json.loads(response.text)
+            flash('The book with ISBN(' + ISBN + ') has been updated successfuly', 'success')
+            return render_template("home.html", book=data, userinfo=userinfo)
+
 
         elif method_delete == "DELETE":
             id = request.form['Id']
-            response = requests.delete("http://127.0.0.1:5000/book/" + id)
+            requests.delete("http://127.0.0.1:5000/book/" + id)
+            response = requests.get("http://127.0.0.1:5000/book")
+            data = json.loads(response.text)
+            flash('The book with ISBN(' + ISBN + ') has been deleted successfuly', 'success')
+            return render_template("home.html", book=data, userinfo=userinfo)
 
         else:
-
-            Title = request.form["Title"]
-            Author = request.form["Author"]
-            PublishedDate = request.form["PublishedDate"]
-            ISBN = request.form["ISBN"]
-
-
-            if len(Title)>100 or len(Author)>50:
+            if len(Title) > 100 or len(Author) > 50:
                 flash('The Title or Author is too long! 1. Modify your input. 2. Contact IT suport.', 'danger')
                 return render_template("add.html")
 
@@ -86,26 +103,38 @@ def index():
                 flash('The Book has added success!', 'success')
                 return render_template("add.html")
 
-        response = requests.get("http://127.0.0.1:5000/book")
-        data = json.loads(response.text)
-        return render_template("home.html", book=data)
+        # response = requests.get("http://127.0.0.1:5000/book")
+        # data = json.loads(response.text)
+        # return render_template("home.html", book=data, userinfo=json.dumps(userinfo))
 
     if request.method == 'GET':
+
+        if session.get('username') == 'xiaoyu' and session.get('password') == '111':
+            userinfo = 'xiaoyu'
+        else:
+            userinfo = None
+
         response = requests.get("http://127.0.0.1:5000/book")
         data = json.loads(response.text)
-        return render_template("home.html", book=data)
+        return render_template("home.html", book=data, userinfo=userinfo)
 
 
 @site.route("/add", methods=['POST','GET'])
 def add():
-    return render_template("add.html")
+    userinfo = isLogin()
+    if request.method == 'POST':
+        print(session.get('username'))
+        print(session.get('password'))
+
+    return render_template("add.html", userinfo=userinfo)
 
 
 @site.route("/report", methods=['POST','GET'])
 def report():
+    userinfo = isLogin()
     response = requests.get("http://127.0.0.1:5000/book")
     data = json.loads(response.text)
-    return render_template("report.html", people=data)
+    return render_template("report.html", people=data, userinfo=userinfo)
 
 
 @site.route("/login/", methods=['GET','POST'])
@@ -113,22 +142,46 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.username.data == 'xiaoyu' and form.password.data == '111':
+            session['username'] = form.username.data
+            session['password'] = form.password.data
             flash('You have been logged in!', 'success')
-            return render_template("home.html")
+            response = requests.get("http://127.0.0.1:5000/book")
+            data = json.loads(response.text)
+            userinfo = form.username.data
+            return render_template("home.html", book=data, userinfo=userinfo)
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+
+    userinfo = isLogin()
+    return render_template('login.html', title='Login', form=form, userinfo=userinfo)
 
 
 @site.route("/about")
 def about():
-    return render_template("about.html", group=group)
+    userinfo = isLogin()
+    return render_template("about.html", group=group, userinfo=userinfo)
 
+
+@site.route("/logout")
+def logout():
+    session.pop('username',None)
+    session.pop('password',None)
+    return redirect(url_for('site.login'))
 
 @site.route("/register")
 def register():
+    userinfo = isLogin()
     form = RegistrationForm()
     if form.validate_on_submit():
         flash('Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+        return render_template('login.html', title='Login', form=form, userinfo=userinfo)
+    return render_template('register.html', title='Register', form=form, userinfo=userinfo)
+
+
+def isLogin():
+    if session.get('username') == 'xiaoyu' and session.get('password') == '111':
+        userinfo = 'xiaoyu'
+    else:
+        userinfo = None
+
+    return userinfo
